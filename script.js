9322b8543aec5a9f284679cfea4b352d17aaf746
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-03-31 16:39:46 -0400

    MVP Complete

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 const playBtn = document.querySelector("#play-btn");
 const gameBoard = document.querySelector("#game-board");
 const whoIsPlaying = document.querySelector("#turn-counter h3");
-let boardClasses;
+let playerOneScore = document.querySelector("#player-one span");
+let playerTwoScore = document.querySelector("#player-two span");
+let gameLog = document.querySelector(".log");
+// let boardClasses;
+let winner = "";
 
 // Player Objects -----------------------------------------
 let playerOne = {
@@ -15,8 +19,6 @@ let playerTwo = {
    spotsFilled: []
 };
 
-// Declare normal variables -------------------------------
-let currentPlayer = playerOne.name;
 const winningCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -26,36 +28,54 @@ const winningCombos = [
 	[2, 5, 8],
 	[0, 4, 8],
 	[2, 4, 6]
-];
+]
 
-// Check player objects grid locations and see if they determine a winner
-if(playerOne.spotsFilled.length >= 3) {
-   playerOne.spotsFilled.sort();
-   console.log(playerOne.spotsFilled);
-} else if(playerTwo.spotsFilled.length >= 3) {
-   playerTwo.spotsFilled.sort();
-   console.log(playerOne.spotsFilled);
-}
+// Declare normal variables -------------------------------
+let currentPlayer = playerOne.name;
+
+let squares = [];
 
 // Functions ----------------------------------------------
 
 // Make Game Board
 function makeBoard(event) {
    event.preventDefault();
-   let divs = [];
    for (let i = 0; i < 9; i++) {
-      divs = document.createElement("div");
+      let divs = document.createElement("div");
       divs.setAttribute("id", i);
       divs.classList.add("square");
       divs.style.cssText = "height: 9rem; width: 9rem; border: 2px solid #1d1e3d";
       gameBoard.appendChild(divs);
       divs.addEventListener("click", fillBox, {once: true});
-      divs.addEventListener("click", addClass, {once: true});
       divs.addEventListener("click", switchPlayers, {once: true});
+      squares.push(divs);
    }
-   boardClasses = document.querySelectorAll(".square");
-   console.log(boardClasses);
    whoIsPlaying.innerText = `It is ${currentPlayer} turn`;
+}
+
+// Check for the winner
+function checkWin() {
+   if(playerOne.spotsFilled.length > 2) {
+      for(arr in winningCombos) {
+         if(playerOne.spotsFilled.includes(winningCombos[arr][0]) && playerOne.spotsFilled.includes(winningCombos[arr][1])  && playerOne.spotsFilled.includes(winningCombos[arr][2])) {
+            console.log("Winner P1");
+            winner = playerOne.name;
+            removeEvent();
+         } else if(playerTwo.spotsFilled.includes(winningCombos[arr][0]) && playerTwo.spotsFilled.includes(winningCombos[arr][1])  && playerTwo.spotsFilled.includes(winningCombos[arr][2])) {
+            console.log("Winner P2");
+            winner = playerTwo.name;
+            removeEvent();
+         }
+      }
+   }
+   whoIsPlaying.innertext = winner;
+}
+
+function removeEvent() {
+   for(let i = 0; i < squares.length; i++) {
+      squares[i].removeEventListener("click", fillBox);
+      squares[i].removeEventListener("click", switchPlayers);
+   }
 }
 
 // Hide play button at beginning
@@ -69,27 +89,20 @@ function fillBox(event) {
       event.target.style.backgroundImage = "url(./Images/X-image.jpg)";
       event.target.style.backgroundSize = "cover";
       event.target.style.backgroundRepeat = "no-repeat";
-      playerOne.spotsFilled.push(event.target.id);
+      playerOne.spotsFilled.push(parseInt(event.target.id));
    } else if(currentPlayer === playerTwo.name){ 
       event.target.style.backgroundImage = "url(./Images/O-image.jpg)";
       event.target.style.backgroundSize = "cover";
       event.target.style.backgroundRepeat = "no-repeat";
-      playerTwo.spotsFilled.push(event.target.id);
+      playerTwo.spotsFilled.push(parseInt(event.target.id));
    } else {
       console.log("Can no longer click to change this");
    }
+   checkWin();
 }
 
 // Assign grid spots to their respective class when clicked
-function addClass(event) {
-   if(currentPlayer === playerOne.name) {
-      event.target.classList.add("X");
-      event.target.classList.remove("O");
-   } else if(currentPlayer === playerTwo.name) {
-      event.target.classList.add("O");
-      event.target.classList.remove("X");
-   }
-}
+
 
 // Switch players when currentPlayer clicks on an empty grid location
 function switchPlayers() {
@@ -103,13 +116,17 @@ function switchPlayers() {
    whoIsPlaying.innerText = `It is ${currentPlayer}'s turn`;
 }
 
-function checkForId() {
-   console.log(playerOne.spotsFilled);
-   console.log(playerTwo.spotsFilled);
-}
-
 // Events -------------------------------------------------
 playBtn.addEventListener("click", makeBoard);
 playBtn.addEventListener("click", hideBtn);
 
-gameBoard.addEventListener("click", checkForId);
+// gameBoard.addEventListener("click", function(event) {
+//    console.log(playerOne.spotsFilled.length);
+//    console.log(playerOne.spotsFilled);
+//    console.log(playerTwo.spotsFilled.length);
+//    console.log(playerTwo.spotsFilled);
+// });
+
+
+// gameBoard.addEventListener("click", checkWin);
+
